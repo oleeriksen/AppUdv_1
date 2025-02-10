10ec3129ea892cd7e5970a41a2e615527b53f907
oleeriksen <oleeriksen1958@gmail.com>
2025-02-10 13:06:09 +0100

    div

---
+++ Modul4/Opgave2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Modul4
+{
+    public class Opgave2
+    {
+        public void Run()
+        {
+            List<int> tal = new();
+            Console.Write("Indtast karaktere (stop ved at skrive stop): ");
+
+            while (true) {
+                string input = Console.ReadLine();
+                if ( input.ToLower() == "stop" )
+                    break;
+                int aGrade = int.Parse(input);
+                tal.Add(aGrade);
+            }
+         
+            Console.WriteLine($"Gennemsnit er: {Average(tal.ToArray())}");
+            int[] grades = { -3, 0, 2, 4, 7, 10, 12 };
+
+            foreach (int aGrade in grades)
+            {
+                int cc = Count(tal, aGrade);
+                if (cc > 0)
+                    Console.WriteLine($"Antal af {aGrade} er {cc}");
+            }
+
+
+        }
+
+        //Returnerer gennemsnittet af tallene i a - NIY
+        double Average(int[] a)
+        {
+            double sum = 0;
+            foreach (int value in a)
+                sum += value;
+            return sum / a.Length;
+        }
+
+        int Count(List<int> a, int key)
+        {
+            int result = 0;
+            foreach (int value in a)
+                if (value == key)
+                    result++;
+            return result;
+        }
+
+    }
+}
+
---
+++ Modul4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Modul4;
+
+Console.WriteLine("Hello, World!");
+
+new Opgave2().Run();
+
