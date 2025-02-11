5f9c6a8adcafd38c188500d593b55ebc32535691
oleeriksen <oleeriksen1958@gmail.com>
2025-02-11 08:19:55 +0100

    opg 5.1

---
+++ Modul5/Opgave 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Modul5
+{
+    public class Opgave_1
+    {
+        public void Run() {
+            Dice d = new();
+
+            int[] count = new int[6];
+            // antal x'ere ligger i count[x-1]
+
+            long N = 6000000000;
+
+            for (long i = 0; i < N; i++) {
+                d.Roll();
+                int idx = d.GetEyes() - 1;
+                count[idx]++;
+            }
+
+            for (int idx = 0; idx < 6; idx++)
+                Console.WriteLine($"Antal {idx+1}'ere: {count[idx]}");
+        }
+    }
+}
+
--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Modul5;
+
 Console.WriteLine("Hello - modul 5");
 
-Modul5.TestDice td = new();
+Opgave_1 td = new();
 
 td.Run();
 
